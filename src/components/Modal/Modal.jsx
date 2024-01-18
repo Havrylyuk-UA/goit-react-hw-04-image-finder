56a5f31bdbaf6c5ac1c106154381bfdc53387da6
Havrylyuk-UA <142251279+Havrylyuk-UA@users.noreply.github.com>
2024-01-18 17:58:29 +0200

    refactor Modal component v1.1

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,20 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 import { createPortal } from 'react-dom';
 
 const modalRoot = document.getElementById('modal-root');
 
 const Modal = ({ close, src, alt }) => {
-  const closeModal = ({ target, currentTarget, code }) => {
-    if (target === currentTarget || code === 'Escape') {
-      close();
-    }
-  };
+  const closeModal = useCallback(
+    ({ target, currentTarget, code }) => {
+      if (target === currentTarget || code === 'Escape') {
+        close();
+      }
+    },
+    [close]
+  );
 
   useEffect(() => {
     document.addEventListener('keydown', closeModal);
 
     return () => document.removeEventListener('keydown', closeModal);
-  }, []);
+  }, [closeModal]);
 
   return createPortal(
     <div className="Overlay" onClick={closeModal}>
