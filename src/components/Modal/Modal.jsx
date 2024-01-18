5de33bd9de7a3d5738ee31d19934cac71262e220
Havrylyuk-UA <142251279+Havrylyuk-UA@users.noreply.github.com>
2024-01-18 17:47:39 +0200

    refactor Modal component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,34 +1,29 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 const modalRoot = document.getElementById('modal-root');
 
-class Modal extends Component {
-  componentDidMount() {
-    document.addEventListener('keydown', this.closeModal);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.closeModal);
-  }
-
-  closeModal = ({ target, currentTarget, code }) => {
+const Modal = ({ close, src, alt }) => {
+  const closeModal = ({ target, currentTarget, code }) => {
     if (target === currentTarget || code === 'Escape') {
-      this.props.close();
+      close();
     }
   };
 
-  render() {
-    const { close, src, alt } = this.props;
-    return createPortal(
-      <div className="Overlay" onClick={this.closeModal}>
-        <div className="Modal">
-          <img src={src} alt={alt} onClick={close} />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+  useEffect(() => {
+    document.addEventListener('keydown', closeModal);
+
+    return () => document.removeEventListener('keydown', closeModal);
+  }, []);
+
+  return createPortal(
+    <div className="Overlay" onClick={closeModal}>
+      <div className="Modal">
+        <img src={src} alt={alt} onClick={close} />
+      </div>
+    </div>,
+    modalRoot
+  );
+};
 
 export default Modal;
